8f439e8421b65b645992196f52d5d37677ec9b4c
pebers <pebers@thoughtmachine.net>
2017-03-30 10:56:20 +0100

    starting on go get thing

--- tools/please_go_tool/main.go
+++ tools/please_go_tool/main.go
@@ -7,11 +7,13 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"gopkg.in/op/go-logging.v1"
 
 	"cli"
+	"tools/please_go_tool/remote"
 	"tools/please_go_tool/testmain"
 )
 
@@ -19,15 +21,25 @@ var log = logging.MustGetLogger("plz_go_test")
 
 var opts = struct {
 	Usage     string
-	Dir       string   `short:"d" long:"dir" description:"Directory to search for Go package files for coverage"`
-	Verbosity int      `short:"v" long:"verbose" default:"1" description:"Verbosity of output (higher number = more output, default 1 -> warnings and errors only)"`
-	Exclude   []string `short:"x" long:"exclude" default:"third_party/go" description:"Directories to exclude from search"`
-	Output    string   `short:"o" long:"output" description:"Output filename" required:"true"`
-	Package   string   `short:"p" long:"package" description:"Package containing this test" env:"PKG"`
-	Args      struct {
-		Go      string   `positional-arg-name:"go" description:"Location of go command" required:"true"`
-		Sources []string `positional-arg-name:"sources" description:"Test source files" required:"true"`
-	} `positional-args:"true" required:"true"`
+	Dir       string `short:"d" long:"dir" description:"Directory to search for Go package files for coverage"`
+	Verbosity int    `short:"v" long:"verbose" default:"2" description:"Verbosity of output (higher number = more output, default 2 -> notice, warnings and errors only)"`
+	Go        string `short:"g" long:"go" default:"go" description:"Go binary to run"`
+
+	TestMain struct {
+		Exclude []string `short:"x" long:"exclude" default:"third_party/go" description:"Directories to exclude from search"`
+		Output  string   `short:"o" long:"output" description:"Output filename" required:"true"`
+		Package string   `short:"p" long:"package" description:"Package containing this test" env:"PKG"`
+		Args    struct {
+			Sources []string `positional-arg-name:"sources" description:"Test source files" required:"true"`
+		} `positional-args:"true" required:"true"`
+	} `command:"testmain" description:"Templates a test main."`
+
+	Remote struct {
+		Args struct {
+			ShortFormat bool     `short:"s" long:"short_format" description:"Prints a shorter format that is used for deriving individual generated rules."`
+			Packages    []string `positional-arg-name:"packages" description:"Packages to fetch" required:"true"`
+		} `positional-args:"true" required:"true"`
+	} `command:"remote" description:"Gets and prints some remote libraries."`
 }{
 	Usage: `
 please_go_tool implements various tools that Please finds useful for building Go code.
@@ -36,18 +48,30 @@ Firstly, it implements a code templater for Go tests.
 This is essentially equivalent to what 'go test' does but it lifts some restrictions
 on file organisation and allows the code to be instrumented for coverage as separate
 build targets rather than having to repeat it for every test.
+
+It also implements a dependency fetcher, akin to a Pleaseish version of go get, which
+generates a bunch of build rules for separate dependencies (and at runtime is re-invoked to
+find source files etc).
 `,
 }
 
 func main() {
-	cli.ParseFlagsOrDie("plz_go_test", "7.2.0", &opts)
+	parser := cli.ParseFlagsOrDie("please_go_tool", "7.9.0", &opts)
 	cli.InitLogging(opts.Verbosity)
-	coverVars, err := testmain.FindCoverVars(opts.Dir, opts.Exclude, opts.Args.Sources)
-	if err != nil {
-		log.Fatalf("Error scanning for coverage: %s", err)
+
+	if parser.Active.Name == "testmain" {
+		coverVars, err := testmain.FindCoverVars(opts.Dir, opts.Exclude, opts.Args.Sources)
+		if err != nil {
+			log.Fatalf("Error scanning for coverage: %s", err)
+		}
+		if err = testmain.WriteTestMain(opts.Package, testmain.IsVersion18(opts.Args.Go), opts.Args.Sources, opts.Output, coverVars); err != nil {
+			log.Fatalf("Error writing test main: %s", err)
+		}
+	} else if parser.Active.Name == "remote" {
+		s, err := remote.FetchLibraries(opts.Go, opts.Remote.ShortFormat, opts.Remote.Packages...)
+		if err != nil {
+			log.Fatalf("%s\n", err)
+		}
+		fmt.Print(s)
 	}
-	if err = testmain.WriteTestMain(opts.Package, testmain.IsVersion18(opts.Args.Go), opts.Args.Sources, opts.Output, coverVars); err != nil {
-		log.Fatalf("Error writing test main: %s", err)
-	}
-	os.Exit(0)
 }
