2854008d60bc30740ce74f18ad9a33821047216f
pebers <pebers@thoughtmachine.net>
2017-03-31 10:28:45 +0100

    Nominal support for specific outs, let's see how this goes...

--- src/core/file_label.go
+++ src/core/file_label.go
@@ -3,6 +3,7 @@
 package core
 
 import "path"
+import "strings"
 
 // FileLabel represents a file in the current package which is directly used by a target.
 type FileLabel struct {
@@ -55,4 +56,40 @@ func (label SystemFileLabel) Label() *BuildLabel {
 
 func (label SystemFileLabel) String() string {
 	return label.Path
+}
+
+// BuildOutputLabel represents a single output file from a particular build rule.
+type BuildOutputLabel struct {
+	BuildLabel
+	Output string
+}
+
+func (label BuildOutputLabel) Paths(graph *BuildGraph) []string {
+	return []string{path.Join(label.PackageName, label.Output)}
+}
+
+func (label BuildOutputLabel) FullPaths(graph *BuildGraph) []string {
+	return []string{path.Join(graph.TargetOrDie(label.BuildLabel).OutDir(), label.Output)}
+}
+
+func (label BuildOutputLabel) LocalPaths(graph *BuildGraph) []string {
+	return []string{label.Output}
+}
+
+func (label BuildOutputLabel) Label() *BuildLabel {
+	return &label.BuildLabel
+}
+
+func (label BuildOutputLabel) String() string {
+	return label.BuildLabel.String() + "|" + label.Output
+}
+
+// TryParseBuildOutputLabel attempts to parse a build output label. It's allowed to just be
+// a normal build label as well.
+func TryParseBuildOutputLabel(target, currentPath string) (BuildInput, error) {
+	if index := strings.IndexRune(target, '|'); index != -1 {
+		label, err := TryParseBuildLabel(target[:index], currentPath)
+		return BuildOutputLabel{BuildLabel: label, Output: target[index+1:]}, err
+	}
+	return TryParseBuildLabel(target, currentPath)
 }
